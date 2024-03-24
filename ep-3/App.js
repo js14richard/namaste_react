288fb59880576f2545f215e96bf8d3c011eea19a
js14richard <js14richard@gmail.com>
2024-03-24 19:57:23 +0530

    ep-3

---
+++ ep-3/App.js
@@ -0,0 +1,17 @@
+
+// React.CreateElement creates the React element (JS-Object) then the render method converts it into HTML 
+
+const heading = React.createElement("h1", {
+    id:"heading", className:"primary_heading"
+}, "Hello World from React")
+console.log(heading)
+
+
+// JSX => Parcel => Babel (compiles/transpiles) => React element (JS-Object) then the render method converts it into HTML 
+
+const jsxHeading = <h1 id="jsxHeading">This is a heading from JSX </h1>
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+console.log(root)
+
+root.render(jsxHeading) 
